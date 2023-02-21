0bd7620aca00038d39b1510757358689c14d3e96
Antoine Griffard <agriffard@hotmail.com>
2023-02-21 09:47:09 +0100

    Kanji

---
+++ src/Sensei/Data/JishoHttpClient.cs
@@ -0,0 +1,43 @@
+﻿using JishoNET.Models;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace Sensei.Data;
+
+public class JishoHttpClient
+{
+    private readonly HttpClient http;
+
+    public JishoHttpClient(HttpClient http)
+    {
+        this.http = http;
+    }
+
+    private static readonly string baseUrl = new("https://jisho.org/api/v1/search/words?keyword=");
+
+    public async Task<JishoResult<JishoDefinition[]>> GetDefinitionAsync(string keyword)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage { RequestUri = new Uri(baseUrl + keyword), Method = HttpMethod.Get };
+            httpRequestMessage.SetBrowserRequestMode(BrowserRequestMode.NoCors);
+            HttpResponseMessage response = await client.SendAsync(httpRequestMessage); //client.GetAsync(baseUrl + keyword);
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            //JishoResult<JishoDefinition[]> result = await client.GetFromJsonAsync<JishoResult<JishoDefinition[]>>(baseUrl + keyword);
+            JishoResult<JishoDefinition[]> result = JsonSerializer.Deserialize<JishoResult<JishoDefinition[]>>(stringResponse);
+            result.Meta.Status = ((int)response.StatusCode);
+            result.Success = true;
+            return result;
+        }
+        catch (Exception e)
+        {
+            return new JishoResult<JishoDefinition[]>
+            {
+                Success = false,
+                Exception = e.ToString()
+            };
+        }
+    }
+}
--- src/Sensei/Program.cs
+++ src/Sensei/Program.cs
@@ -1,11 +1,14 @@
-using Sensei;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Sensei;
+using Sensei.Data;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient<JishoHttpClient>(client => client.BaseAddress = new Uri("https://jisho.org")); 
+builder.Services.AddHttpClient("jisho", client =>  client.BaseAddress = new Uri("https://jisho.org"));
 
 await builder.Build().RunAsync();
