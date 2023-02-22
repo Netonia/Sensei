2c65956473529236fde8f2bcb49f1d901bdcc11a
Antoine Griffard <agriffard@hotmail.com>
2023-02-22 22:06:21 +0100

    Kanji

---
+++ src/Sensei/Data/KanjiResult.cs
@@ -0,0 +1,23 @@
+﻿namespace Sensei.Data;
+
+public class KanjiResult
+{
+    public KanjiResult(string literal, int? jlpt, int strokeCount, string kun, string on, string[] meanings)
+    {
+        Literal = literal;
+        JLPT = jlpt;
+        StrokeCount = strokeCount;
+        Kun = kun;
+        On = on;
+        Meanings = meanings;
+    }
+
+    public string Literal { get; set; }
+    public int? JLPT { get; set; }
+    public int? Frequency { get; set; }
+    public int StrokeCount { get; set; }
+    public string Kun { get; set; }
+    public string On { get; set; }
+    public string[] Meanings { get; set; }
+    //public IEnumerable<string> RadicalDecomposition { get; set; }
+}
