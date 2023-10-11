5f931b8ec09bf2369dfedb056c01a2ce4bdfea5f
Antoine Griffard <agriffard@hotmail.com>
2023-10-11 13:46:20 +0200

    .NET 8

--- src/Sensei/Program.cs
+++ src/Sensei/Program.cs
@@ -1,4 +1,4 @@
-using Sensei;
+using Sensei.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
